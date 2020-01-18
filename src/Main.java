d6b0acdf643e8b1a37f1a129d32654264997a08b
Dominik Simgen <dominiksimgen@Dominiks-MacBook-Pro-2.local>
2020-01-18 23:56:08 +0100

    fourth update to main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("another printline");
         System.out.println("Print line once more");
+        System.out.println("Final line");
     }
 }
